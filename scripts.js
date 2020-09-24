0af333ef7c610ad80a53ccc68c2190fb9c319ded
Syed Imran Abidi <imrans110@hotmail.com>
2020-09-24 13:20:36 +0530

    Show Lyriscs as Accordian Content

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-const API_BASE_URL = "https://api.lyrics.ovh/v1";
+const API_BASE_URL =
+  "https://private-anon-5e7186017e-lyricsovh.apiary-proxy.com/v1/";
 const API_SUGGESTION_URL = "https://api.lyrics.ovh/suggest/";
 
+let activeAccordianID = "";
+
 const createCard = (item) => {
-  return `<div class="card" id=${item.id}>
-    <div><b>${item.artist.name}</b> - <span>${item.title}</span></div>
-    <button class="show-button button" onclick="" type="button">
-      Show Lyrics
-    </button>
-  </div>`;
+  return `<li id='line_${item.id}'>
+            <div class="card" id=${item.id}>
+              <div>
+                <b>${item.artist.name}</b> - 
+                <span>${item.title}</span>
+              </div>
+              <button id='show_btn_${item.id}' class="show-button button" onclick="handleAccordian('${item.id}', '${item.artist.name}', '${item.title}')" type="button">
+                Show Lyrics
+              </button>
+            </div>
+            <div class='panel' id='panel_${item.id}' /> 
+          </li>`;
 };
 
 const getValueById = (id) => document.getElementById(id).value;
@@ -22,9 +31,55 @@ const onSearchButtonClick = () => {
       .then((data) => {
         document.getElementById("list-items").innerHTML = null;
         data.data.forEach((item) => {
-          document.getElementById("list-items").innerHTML +=
-            "<li>" + createCard(item) + "</li>";
+          document.getElementById("list-items").innerHTML += createCard(item);
         });
       });
   }
+};
+
+const getLyrics = (lineID, artistName, trackName) =>
+  new Promise((resolve, reject) => {
+    fetch(API_BASE_URL + "/" + artistName + "/" + trackName)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log({ data });
+        resolve(data);
+      })
+      .catch((e) => {
+        console.log({ e });
+        reject(e);
+      });
+  });
+
+const handleAccordian = async (lineID, artistName, trackName) => {
+  const songLyrics = await getLyrics(lineID, artistName, trackName);
+  const lineItem = document.getElementById(lineID);
+  const panelItem = document.getElementById(`panel_${lineID}`);
+  const currentActiveAccordian = document.getElementById(activeAccordianID);
+  const showButton = document.getElementById(`show_button_${lineID}`);
+
+  // showButton.innerHTML = "Hide Lyrics";
+  // showButton.onclick = function () {
+  //   showButton.innerHTML = "Show Lyrics";
+  //   // panelItem.style.display = "none";
+  //   showButton.onclick = handleAccordian(
+  //     `${lineID}`,
+  //     `${artist}`,
+  //     `${trackName}`
+  //   );
+  // };
+
+  if (currentActiveAccordian) {
+    currentActiveAccordian.style.display = "none";
+    currentActiveAccordian.innerHTML = "";
+  }
+
+  if (songLyrics.lyrics) {
+    panelItem.style.display = "block";
+    panelItem.innerHTML = songLyrics.lyrics;
+  } else {
+    panelItem.style.display = "block";
+    panelItem.innerHTML = "No Lyrics Available";
+  }
+  activeAccordianID = `panel_${lineID}`;
 };
